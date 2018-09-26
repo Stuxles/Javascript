6f5ea2460da33dffc6162c9e4e82786a22e37b7c
Stuxles <stuxles@live.nl>
2018-09-26 22:58:59 +0200

    changes

--- 2A/script.js
+++ 2A/script.js
@@ -10,5 +10,40 @@ class Passenger {
 
 }
 
+class Plane {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHi() {
+    console.log(this.name);
+  }
+
+}
+
+class Airline {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHi() {
+    console.log(this.name);
+  }
+
+}
+
+class Passenger {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHi() {
+    console.log(this.name);
+  }
+
+}
+
 let user = new Passenger("Bob");
-user.sayHi();
